2bc4bf905f7f215ef9bce8c9b045e2cc0d0058dd
Tristan Slominski <tristan.slominski@gmail.com>
2013-07-04 18:51:43 -0500

    implemented determineBucket()

--- index.js
+++ index.js
@@ -28,8 +28,29 @@ var KBucket = module.exports = function KBucket (options) {
 
 util.inherits(KBucket, events.EventEmitter);
 
-KBucket.prototype.add = function add (contact) {
+// contact: *required* the contact object to add
+// id: a Buffer we use to walk the KBucket binary tree
+// bitIndex: the bitIndex to which byte to check in the Buffer for navigating the
+//          binary tree
+KBucket.prototype.add = function add (contact, id, bitIndex) {
     var self = this;
+
+    // console.dir(self);
+
+    // first check whether we are an inner node or a leaf (with bucket contents)
+    if (!self.bucket) {
+        // this is not a leaf node but an inner node with 'low' and 'high'
+        // branches; we will check the appropriate byte of the identifier and
+        // delegate to the appropriate node for further processing
+        id = id || contact.id;
+        bitIndex = bitIndex || 0;
+
+        if (id[bitIndex++] < 0x80) {
+            return self.low.add(contact, id, bitIndex);
+        } else {
+            return self.high.add(contact, id, bitIndex);
+        }
+    }
 
     // check if the contact already exists
     var index = self.indexOf(contact);
@@ -57,11 +78,71 @@ KBucket.prototype.indexOf = function indexOf (contact) {
 // Splits the bucket, redistributes contacts to the new buckets, and marks the
 // bucket that was split as an inner node of the binary tree of buckets by
 // setting self.bucket = undefined;
-KBucket.prototype.splitAndAdd = function splitAndAdd (contact) {
+// contact: *required* the contact object to add
+// id: a Buffer we use to walk the KBucket binary tree
+// bitIndex: the bitIndex to which byte to check in the Buffer for navigating the
+//          binary tree
+KBucket.prototype.splitAndAdd = function splitAndAdd (contact, id, bitIndex) {
     var self = this;
     self.low = new KBucket({localNodeId: self.localNodeId});
     self.high = new KBucket({localNodeId: self.localNodeId});
 
+    id = id || contact.id;
+    bitIndex = bitIndex || 0;
+
+    // redistribute existing contacts amongst the two newly created buckets
+    self.bucket.forEach(function (storedContact) {
+        // TODO: add extra check for identifiers that are too short
+        if (storedContact.id[bitIndex] < 0x80) {
+            self.low.add(storedContact);
+        } else {
+            self.high.add(storedContact);
+        }
+    });
+
     self.bucket = undefined; // mark as inner tree node
+    
+    // add the contact being added
+    self.add(contact, id, bitIndex);
+
     return self;
+};
+
+// Determines whether the id at the bitIndex is 0 or 1. If 0, returns -1, else 1
+// id: a Buffer to compare localNodeId with
+// bitIndex: the bitIndex to which bit to check in the id Buffer
+KBucket.prototype.determineBucket = function determineBucket (id, bitIndex) {
+    var self = this;
+
+    bitIndex = bitIndex || 0;
+
+    // **NOTE** remember that id is a Buffer and has granularity of 
+    // bytes (8 bits), whereas the bitIndex is the _bit_ index (not byte)
+
+    // id's that are too short are put in low bucket (1 byte = 8 bits)
+    // parseInt(bitIndex / 8) finds how many bytes the bitIndex describes
+    // bitIndex % 8 checks if we have extra bits beyond byte multiples
+    // if number of bytes is <= no. of bytes described by bitIndex and there
+    // are extra bits to consider, this means id has less bits than what 
+    // bitIndex describes, id therefore is too short, and will be put in low 
+    // bucket
+    var bytesDescribedByBitIndex = parseInt(bitIndex / 8);
+    var bitIndexWithinByte = bitIndex % 8;
+    if ((id.length <= bytesDescribedByBitIndex)
+        && (bitIndexWithinByte != 0)) return -1; 
+
+    var byteUnderConsideration = id[bytesDescribedByBitIndex];
+
+    // byteUnderConsideration is an integer from 0 to 255 represented by 8 bits 
+    // where 255 is 11111111 and 0 is 00000000
+    // in order to find out whether the bit at bitIndexWithinByte is set 
+    // we construct Math.pow(2, (7 - bitIndexWithinByte)) which will consist 
+    // of all bits being 0, with only one bit set to 1
+    // for example, if bitIndexWithinByte is 3, we will construct 00010000 by
+    // Math.pow(2, (7 - 3)) -> Math.pow(2, 4) -> 16
+    if (byteUnderConsideration & Math.pow(2, (7 - bitIndexWithinByte))) {
+        return 1;
+    }
+    
+    return -1;
 };
